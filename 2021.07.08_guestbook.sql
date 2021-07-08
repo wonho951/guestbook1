388722a8a5a8a949eeaa1ca474217642cdefec89
wonho <wonho951@gmail.com>
2021-07-09 00:51:00 +0900

    2021.07.08 mode1과제(guestbook1)

---
+++ 2021.07.08_guestbook.sql
@@ -0,0 +1,52 @@
+--테이블삭제
+drop table guestbook;
+--시퀀스 삭제
+DROP SEQUENCE seq_guestbook_no;
+
+
+
+--테이블 생성
+create table guestbook (
+    no number,
+    name VARCHAR2(80),
+    password VARCHAR2(20),
+    content varchar2(2000),
+    reg_date date,
+    PRIMARY KEY (no)
+);
+
+
+--시퀀스 생성
+create sequence SEQ_GUESTBOOK_NO
+increment by 1
+start with 1;
+
+
+
+
+INSERT INTO guestbook
+values(SEQ_GUESTBOOK_NO.nextval,'이정재','123','안녕하세요',sysdate);
+
+
+INSERT INTO guestbook
+values(SEQ_GUESTBOOK_NO.nextval,'이정재','234','hello',sysdate);
+
+commit;
+
+
+
+delete from guestbook
+where no = 5;
+
+
+select  *
+from guestbook;
+
+select  no,
+        name,
+        password,
+        content,
+        reg_date
+from guestbook;
+
+
